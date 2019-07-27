f65a0faffaa1bc15fe3666ef97771c42cc2906c9
chinmayshah99 <chinmayshah3899@gmail.com>
2019-07-27 20:50:26 +0530

    code cleaning

--- greedybfs/greedybfs.py
+++ greedybfs/greedybfs.py
@@ -1,13 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from priorityQueue import PriorityQueue
-from graph import Graph
+from .priorityQueue import PriorityQueue
+from .graph import Graph
 
-#     test_graph = {
-#         "a": [['s',253], ['t', 329], ['z',374]],
-#         "s": [['f', 176], ['o', 380], ['r', 193]],
-#         "f": [['b', 0]]
-#     }
 
 
 def gbfs(start_node, end_node, graph_input={}):
@@ -18,8 +13,8 @@ def gbfs(start_node, end_node, graph_input={}):
     start_node = start_node
     end_node = end_node
 
-    print(start_node, end='')
-    start = graph.get_node_details(start_node)
+    # print(start_node, end='')
+    # start = graph.get_node_details(start_node)
     current_node = [start_node]
 
     while current_node[0] != end_node:
